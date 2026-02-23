dd336df67fff59ced5508befeba8a32e36af181f
mgarsan719 <margagon154@gmail.com>
2026-02-23 10:14:36 +0100

    excepcion 500 controlado

--- src/main/java/ies/alcores/api_productos/controller/CategoriaController.java
+++ src/main/java/ies/alcores/api_productos/controller/CategoriaController.java
@@ -5,6 +5,7 @@ import ies.alcores.api_productos.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,4 +23,11 @@ public class CategoriaController {
         return ResponseEntity.ok(this.categoriaService.findAll());
     }
 
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Categoria> obtenerPorId(@PathVariable Long id) {
+        return this.categoriaService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
