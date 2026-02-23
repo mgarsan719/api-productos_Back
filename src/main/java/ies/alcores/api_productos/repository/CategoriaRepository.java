2075918a797825feb38d970bfafd0bb1a9038ee1
mgarsan719 <margagon154@gmail.com>
2026-02-23 09:49:37 +0100

    coreccion repositories

--- src/main/java/ies/alcores/api_productos/repository/CategoriaRepository.java
+++ src/main/java/ies/alcores/api_productos/repository/CategoriaRepository.java
@@ -2,8 +2,6 @@ package ies.alcores.api_productos.repository;
 
 import ies.alcores.api_productos.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 }
